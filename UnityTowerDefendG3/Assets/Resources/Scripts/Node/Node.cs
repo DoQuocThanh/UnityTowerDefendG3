763843bd1b12b62df3faf7cc670728b5d9ed4f86
caocong11 <76118203+caocong11@users.noreply.github.com>
2023-10-03 22:17:38 +0700

    Node done button, show gun

--- UnityTowerDefendG3/Assets/Resources/Scripts/Node/Node.cs
+++ UnityTowerDefendG3/Assets/Resources/Scripts/Node/Node.cs
@@ -1,20 +1,42 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Node : MonoBehaviour
 {
-
-    public GameObject Tower;
+    public GameObject[] Towers; // An array of Tower prefabs
+    public GameObject TowerSelectionPopup;
     private bool hasTower = false;
+
+    private void Start()
+    {
+        TowerSelectionPopup.SetActive(false);
+
+        // Setup listeners for each button
+        TowerSelectionPopup.transform.Find("Button1").GetComponent<Button>().onClick.AddListener(() => BuyTower(Towers[0]));
+        TowerSelectionPopup.transform.Find("Button2").GetComponent<Button>().onClick.AddListener(() => BuyTower(Towers[1]));
+        TowerSelectionPopup.transform.Find("Button3").GetComponent<Button>().onClick.AddListener(() => BuyTower(Towers[2]));
+       
+    }
 
     private void OnMouseDown()
     {
-        if(hasTower)
+        if (!hasTower)
         {
-            Instantiate(Tower, transform.position, Quaternion.identity);
-            hasTower = true;
+            Debug.Log(hasTower);
+            ShowTowerSelectionPopup();
         }
+    }
+
+    private void ShowTowerSelectionPopup()
+    {
+        TowerSelectionPopup.SetActive(true);
+    }
+
+    public void BuyTower(GameObject selectedTower)
+    {
+        Instantiate(selectedTower, transform.position, Quaternion.identity);
+        hasTower = true;
+        TowerSelectionPopup.SetActive(false);
     }
 }
