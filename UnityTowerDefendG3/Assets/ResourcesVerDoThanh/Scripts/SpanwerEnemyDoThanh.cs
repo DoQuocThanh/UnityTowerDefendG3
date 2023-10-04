dcc6b508451762e1f06b0eb580d2b2991b949388
DoQuocThanh <thanhdq2302@gmail.com>
2023-10-04 22:27:51 +0700

    update

--- UnityTowerDefendG3/Assets/ResourcesVerDoThanh/Scripts/SpanwerEnemyDoThanh.cs
+++ UnityTowerDefendG3/Assets/ResourcesVerDoThanh/Scripts/SpanwerEnemyDoThanh.cs
@@ -9,15 +9,11 @@ public class SpanwerEnemy : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private GameObject[] enemyPrefabs;
-
-
     [Header("Attributes")]
     [SerializeField] private int baseEmemies = 8;
     [SerializeField] private float eneminiesPerSecond = 0.5f;
     [SerializeField] private float timeBetweenWaves = 5f;
-
     [Header("Events")]
-
     private int currentWave = 1;
     private float timeSinceLastSpawn;
     public static int enemiesAlies;
