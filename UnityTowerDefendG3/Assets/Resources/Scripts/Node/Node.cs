db0e96764a21a5775d3eaee8f3a38a7fa471ce35
DoQuocThanh <thanhdq2302@gmail.com>
2023-10-06 10:23:33 +0700

    update

--- UnityTowerDefendG3/Assets/Resources/Scripts/Node/Node.cs
+++ UnityTowerDefendG3/Assets/Resources/Scripts/Node/Node.cs
@@ -1,42 +1,70 @@
-using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class Node : MonoBehaviour
 {
-    public GameObject[] Towers; // An array of Tower prefabs
+    public GameObject[] Towers;
     public GameObject TowerSelectionPopup;
-    private bool hasTower = false;
+    private GameObject currentTower = null;
 
+    private bool onClick = false;
     private void Start()
     {
         TowerSelectionPopup.SetActive(false);
-        
-        // Setup listeners for each button
         TowerSelectionPopup.transform.Find("Button1").GetComponent<Button>().onClick.AddListener(() => BuyTower(Towers[0]));
         TowerSelectionPopup.transform.Find("Button2").GetComponent<Button>().onClick.AddListener(() => BuyTower(Towers[1]));
         TowerSelectionPopup.transform.Find("Button3").GetComponent<Button>().onClick.AddListener(() => BuyTower(Towers[2]));
-       
+
     }
+
+    public void Print() {
+        Debug.Log("dsadas");
+    }
+
+
+    
 
     private void OnMouseDown()
     {
-        if (!hasTower)
+        onClick = true;
+        if (TowerSelectionPopup.activeSelf)
         {
-            Debug.Log(hasTower);
+            HideTowerSelectionPopup();
+        }
+        else
+        {
             ShowTowerSelectionPopup();
         }
     }
 
     private void ShowTowerSelectionPopup()
     {
+        Vector3 addPos = transform.position + Vector3.up;
+        TowerSelectionPopup.transform.position = addPos;
         TowerSelectionPopup.SetActive(true);
+
+
+    }
+
+    private void HideTowerSelectionPopup()
+    {
+        TowerSelectionPopup.SetActive(false);
     }
 
     public void BuyTower(GameObject selectedTower)
     {
-        Instantiate(selectedTower, transform.position, Quaternion.identity);
-        hasTower = true;
-        TowerSelectionPopup.SetActive(false);
+
+        if (this.onClick) {
+            if (currentTower)
+            {
+                Destroy(currentTower);
+            }
+
+            currentTower = Instantiate(selectedTower, transform.position, Quaternion.identity, this.transform);            
+            onClick = false;
+            HideTowerSelectionPopup();
+        }
+       
     }
 }
