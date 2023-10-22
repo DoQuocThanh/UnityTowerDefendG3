45e2b96e4299b8bf51bdc9c14fdbb21638b323c3
Toanh <anhng130@gmail.com>
2023-10-22 16:05:41 +0700

    add introduction

--- UnityTowerDefendG3/Assets/Resources/Scripts/Wave/WaveSpawner.cs
+++ UnityTowerDefendG3/Assets/Resources/Scripts/Wave/WaveSpawner.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
-using static WaveSpawner;
 
 public class WaveSpawner : MonoBehaviour
 {
+    
     [System.Serializable]
     public class EnemyUnit
     {
@@ -28,24 +28,26 @@ public class WaveSpawner : MonoBehaviour
     public Transform spawnPoint;
     public TextMeshProUGUI waveText;        // Text để hiển thị sóng
     public TextMeshProUGUI remainEnemyText;   // Text mới để hiển thị số lượng quái vật còn lại
-
     private int currentWave = 0;
 
     private void Start()
     {
         //AudioManeger.Instance.PlayMusic("Theme");
-        StartCoroutine(SpawnWaves());
+    
+            StartCoroutine(SpawnWaves());
+      
     }
 
     private IEnumerator SpawnWaves()
     {
-        for (; currentWave < waves.Length; currentWave++)
-        {
-            yield return new WaitForSeconds(waves[currentWave].timeBeforeEnemySpawn);
-            UpdateWaveInfo(currentWave + 1);
-            yield return StartCoroutine(SpawnWave(waves[currentWave]));
-            
-        }
+            Debug.Log("kkkk");
+            for (; currentWave < waves.Length; currentWave++)
+            {
+                yield return new WaitForSeconds(waves[currentWave].timeBeforeEnemySpawn);
+                UpdateWaveInfo(currentWave + 1);
+                yield return StartCoroutine(SpawnWave(waves[currentWave]));
+
+            }
     }
 
     private void Update()
@@ -55,19 +57,21 @@ public class WaveSpawner : MonoBehaviour
 
     private IEnumerator SpawnWave(Wave wave)
     {
-
-        int indexEnemy = 0;
-        float timeRandom = 0;
-        while (wave.enemyUnits.Sum(x => x.quantity) > 0) {
-            indexEnemy = Random.Range(0, wave.enemyUnits.Count());
-            timeRandom = Random.Range(wave.timeBetweenSpawnsMin, wave.timeBetweenSpawnsMax);
-            Instantiate(wave.enemyUnits[indexEnemy].enemy, spawnPoint.position, spawnPoint.rotation);
-            wave.enemyUnits[indexEnemy].quantity -= 1;
-            if(wave.enemyUnits[indexEnemy].quantity == 0)
-            wave.enemyUnits.Remove(wave.enemyUnits[indexEnemy]);
-            yield return new WaitForSeconds(timeRandom);
-        }
-
+       
+            int indexEnemy = 0;
+            float timeRandom = 0;
+            while (wave.enemyUnits.Sum(x => x.quantity) > 0)
+            {
+                indexEnemy = Random.Range(0, wave.enemyUnits.Count());
+                timeRandom = Random.Range(wave.timeBetweenSpawnsMin, wave.timeBetweenSpawnsMax);
+                Instantiate(wave.enemyUnits[indexEnemy].enemy, spawnPoint.position, spawnPoint.rotation);
+                wave.enemyUnits[indexEnemy].quantity -= 1;
+                if (wave.enemyUnits[indexEnemy].quantity == 0)
+                    wave.enemyUnits.Remove(wave.enemyUnits[indexEnemy]);
+                yield return new WaitForSeconds(timeRandom);
+            }
+       
+       
     }
 
     private void UpdateWaveInfo(int waveNumber)
